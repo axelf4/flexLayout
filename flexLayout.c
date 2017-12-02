25d99f4529ad467b8731720309c31353abe55a28
Axel Forsman <axelsfor@gmail.com>
2017-12-02 17:17:51 +0100

    Cleanup the code and rename LayoutContext.

--- flexLayout.c
+++ flexLayout.c
@@ -2,72 +2,63 @@
 
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 
-int isUndefined(const float value) {
+int isUndefined(float value) {
 	return isnan(value);
 }
 
-static enum FlexDirection getPerpendicularAxis(const enum FlexDirection axis) {
+static enum FlexDirection getPerpendicularAxis(enum FlexDirection axis) {
 	return axis == DIRECTION_ROW ? DIRECTION_COLUMN : DIRECTION_ROW;
 }
 
-static float getLeadingMargin(const struct FlexParams *params, const enum FlexDirection axis) {
+static float getLeadingMargin(struct FlexParams *params, enum FlexDirection axis) {
 	return axis == DIRECTION_ROW ? params->marginLeft : params->marginTop;
 }
 
-static float getTrailingMargin(const struct FlexParams *params, const enum FlexDirection axis) {
+static float getTrailingMargin(struct FlexParams *params, enum FlexDirection axis) {
 	return axis == DIRECTION_ROW ? params->marginRight : params->marginBottom;
 }
 
-static float getMargin(const struct FlexParams *params, const enum FlexDirection axis) {
+static float getMargin(struct FlexParams *params, enum FlexDirection axis) {
 	return getLeadingMargin(params, axis) + getTrailingMargin(params, axis);
 }
 
-static float getStyleSize(const struct FlexParams *params, const enum FlexDirection axis) {
+static float getStyleSize(struct FlexParams *params, enum FlexDirection axis) {
 	return axis == DIRECTION_ROW ? params->width : params->height;
 }
 
-static float getLayoutSize(const struct LayoutContext *context, const void *widget, enum FlexDirection axis) {
+static float getLayoutSize(const struct FlexContext *context, void *widget, enum FlexDirection axis) {
 	return axis == DIRECTION_ROW ? context->getWidth(widget) : context->getHeight(widget);
 }
 
-static Align getAlign(const struct FlexParams *params) {
-	return params->align;
+static int isFlexBasisAuto(struct FlexParams *params) {
+	return params->flex <= 0;
 }
 
-static float getFlex(const struct FlexParams *params) {
-	return params->flex;
-}
-
-static int isFlexBasisAuto(const struct FlexParams *params) {
-	return getFlex(params) <= 0;
-}
-
-static float getFlexGrowFactor(const struct FlexParams *params) {
-	float flex = getFlex(params);
+static float getFlexGrowFactor(struct FlexParams *params) {
+	float flex = params->flex;
 	if (flex > 0) return flex;
 	return 0;
 }
 
-static int getFlexShrinkFactor(const struct FlexParams *params) {
-	return getFlex(params) < 0;
+static int getFlexShrinkFactor(struct FlexParams *params) {
+	return params->flex < 0;
 }
 
-void layoutFlex(const struct LayoutContext *layoutContext, const void *widget, float width, MeasureMode widthMode, float height, MeasureMode heightMode, const FlexDirection direction, const Align justify) {
-	const enum FlexDirection mainAxis = direction, crossAxis = getPerpendicularAxis(mainAxis);
-	const int childCount = layoutContext->getChildCount(widget);
-	const MeasureMode mainMeasureMode = mainAxis == DIRECTION_ROW ? widthMode : heightMode,
-		  crossMeasureMode = crossAxis == DIRECTION_ROW ? widthMode : heightMode;
-	const float availableMain = mainAxis == DIRECTION_ROW ? width : height,
+void layoutFlex(const struct FlexContext *context, void *widget, float width, enum MeasureMode widthMode, float height, enum MeasureMode heightMode, enum FlexDirection direction, enum Align justify) {
+	enum FlexDirection mainAxis = direction, crossAxis = getPerpendicularAxis(mainAxis);
+	int childCount = context->getChildCount(widget);
+	enum MeasureMode mainMeasureMode = mainAxis == DIRECTION_ROW ? widthMode : heightMode,
+					 crossMeasureMode = crossAxis == DIRECTION_ROW ? widthMode : heightMode;
+	float availableMain = mainAxis == DIRECTION_ROW ? width : height,
 		  availableCross = crossAxis == DIRECTION_ROW ? width : height;
 
 	// Determine basis for each child
 	float sizeConsumed = 0, // Dimensions of the content in the main axis
 		  totalFlexGrowFactors = 0, totalFlexShrinkScaledFactors = 0;
 	for (int i = 0; i < childCount; ++i) {
-		const void *child = layoutContext->getChildAt(widget, i);
-		const struct FlexParams *params = layoutContext->getLayoutParams(child);
-		const float styleSize = getStyleSize(params, mainAxis);
-		float basis;
+		void *child = context->getChildAt(widget, i);
+		struct FlexParams *params = context->getLayoutParams(child);
+		float styleSize = getStyleSize(params, mainAxis), basis;
 
 		if (!isUndefined(styleSize)) {
 			basis = styleSize;
@@ -75,14 +66,13 @@ void layoutFlex(const struct LayoutContext *layoutContext, const void *widget, f
 			basis = 0;
 		} else {
 			// Determine the base size by performing layout
-			const Align align = getAlign(params);
 			float childWidth, childHeight;
-			MeasureMode childWidthMode, childHeightMode;
+			enum MeasureMode childWidthMode, childHeightMode;
 
 			if (!isUndefined(getStyleSize(params, DIRECTION_ROW))) {
 				childWidth = params->width;
 				childWidthMode = MEASURE_EXACTLY;
-			} else if (crossAxis == DIRECTION_ROW && widthMode == MEASURE_EXACTLY && align == ALIGN_STRETCH) {
+			} else if (crossAxis == DIRECTION_ROW && widthMode == MEASURE_EXACTLY && params->align == ALIGN_STRETCH) {
 				childWidth = width;
 				childWidthMode = MEASURE_EXACTLY;
 			} else {
@@ -93,7 +83,7 @@ void layoutFlex(const struct LayoutContext *layoutContext, const void *widget, f
 			if (!isUndefined(getStyleSize(params, DIRECTION_COLUMN))) {
 				childHeight = params->height;
 				childHeightMode = MEASURE_EXACTLY;
-			} else if (crossAxis == DIRECTION_COLUMN && heightMode == MEASURE_EXACTLY && align == ALIGN_STRETCH) {
+			} else if (crossAxis == DIRECTION_COLUMN && heightMode == MEASURE_EXACTLY && params->align == ALIGN_STRETCH) {
 				childHeight = height;
 				childHeightMode = MEASURE_EXACTLY;
 			} else {
@@ -101,22 +91,23 @@ void layoutFlex(const struct LayoutContext *layoutContext, const void *widget, f
 				childHeightMode = heightMode == MEASURE_UNSPECIFIED ? MEASURE_UNSPECIFIED : MEASURE_AT_MOST;
 			}
 
-			layoutContext->layout(child, childWidth, childWidthMode, childHeight, childHeightMode);
-			basis = getLayoutSize(layoutContext, child, mainAxis);
+			context->layout(child, childWidth, childWidthMode, childHeight, childHeightMode);
+			basis = getLayoutSize(context, child, mainAxis);
 		}
 
-		layoutContext->setWidth(child, basis); // Store the basis in the child's width dimension
+		context->setWidth(child, basis); // Store the basis in the child's width dimension
 		sizeConsumed += basis + getMargin(params, mainAxis);
 		totalFlexGrowFactors += getFlexGrowFactor(params);
 		totalFlexShrinkScaledFactors += getFlexShrinkFactor(params) * basis;
 	}
 
 	// Layout flexible children and allocate empty space
-	const float remainingSpace = availableMain ? availableMain - sizeConsumed : 0; // The remaining available space in the main axis
+	float remainingSpace = availableMain ? availableMain - sizeConsumed : 0; // The remaining available space in the main axis
 	float leadingMainSize = 0, betweenMain = 0;
 	if (totalFlexGrowFactors == 0 && remainingSpace > 0 && mainMeasureMode == MEASURE_EXACTLY) {
 		// Allocate remaining space according to justify.
 		switch (justify) {
+			default:
 			case ALIGN_START:
 				break;
 			case ALIGN_CENTER:
@@ -135,29 +126,29 @@ void layoutFlex(const struct LayoutContext *layoutContext, const void *widget, f
 	}
 	int mainSize = leadingMainSize, crossSize = 0;
 	for (int i = 0; i < childCount; ++i) {
-		const void *child = layoutContext->getChildAt(widget, i);
-		const struct FlexParams *params = layoutContext->getLayoutParams(child);
-		const float childCrossStyleSize = getStyleSize(params, crossAxis);
-		float childBasis = layoutContext->getWidth(child);
+		void *child = context->getChildAt(widget, i);
+		struct FlexParams *params = context->getLayoutParams(child);
+		float childCrossStyleSize = getStyleSize(params, crossAxis);
+		float childBasis = context->getWidth(child);
 
 		if (remainingSpace < 0) {
-			const float flexShrinkScaledFactor = getFlexShrinkFactor(params) * childBasis;
+			float flexShrinkScaledFactor = getFlexShrinkFactor(params) * childBasis;
 			if (flexShrinkScaledFactor != 0) childBasis += remainingSpace / totalFlexShrinkScaledFactors * flexShrinkScaledFactor;
 		} else if (remainingSpace > 0) {
-			const float flexGrowFactor = getFlexGrowFactor(params);
+			float flexGrowFactor = getFlexGrowFactor(params);
 			if (flexGrowFactor != 0) childBasis += remainingSpace / totalFlexGrowFactors * flexGrowFactor;
 		}
 
-		const float childCrossSize = isUndefined(childCrossStyleSize) ? availableCross : childCrossStyleSize;
-		const MeasureMode childCrossMode = !isUndefined(childCrossStyleSize) || crossMeasureMode == MEASURE_EXACTLY && getAlign(params) == ALIGN_STRETCH
-				  ? MEASURE_EXACTLY : crossMeasureMode == MEASURE_UNSPECIFIED ? MEASURE_UNSPECIFIED : MEASURE_AT_MOST;
-		if (mainAxis == DIRECTION_ROW) layoutContext->layout(child, childBasis, MEASURE_EXACTLY, childCrossSize, childCrossMode);
-		else layoutContext->layout(child, childCrossSize, childCrossMode, childBasis, MEASURE_EXACTLY);
+		float childCrossSize = isUndefined(childCrossStyleSize) ? availableCross : childCrossStyleSize;
+		enum MeasureMode childCrossMode = !isUndefined(childCrossStyleSize) || (crossMeasureMode == MEASURE_EXACTLY && params->align == ALIGN_STRETCH)
+			? MEASURE_EXACTLY : crossMeasureMode == MEASURE_UNSPECIFIED ? MEASURE_UNSPECIFIED : MEASURE_AT_MOST;
+		if (mainAxis == DIRECTION_ROW) context->layout(child, childBasis, MEASURE_EXACTLY, childCrossSize, childCrossMode);
+		else context->layout(child, childCrossSize, childCrossMode, childBasis, MEASURE_EXACTLY);
 
-		// Position elements in the main axis
-		(mainAxis == DIRECTION_ROW ? layoutContext->setX : layoutContext->setY)(child, mainSize + getLeadingMargin(params, mainAxis));
-		mainSize += betweenMain + getLayoutSize(layoutContext, child, mainAxis) + getMargin(params, mainAxis);
-		crossSize = MAX(crossSize, getLayoutSize(layoutContext, child, crossAxis) + getMargin(params, crossAxis));
+		// Position element in the main axis
+		(mainAxis == DIRECTION_ROW ? context->setX : context->setY)(child, mainSize + getLeadingMargin(params, mainAxis));
+		mainSize += betweenMain + getLayoutSize(context, child, mainAxis) + getMargin(params, mainAxis);
+		crossSize = MAX(crossSize, getLayoutSize(context, child, crossAxis) + getMargin(params, crossAxis));
 	}
 
 	// If the dimensions are definite: set them
@@ -166,28 +157,29 @@ void layoutFlex(const struct LayoutContext *layoutContext, const void *widget, f
 
 	// Position elements in the cross axis
 	for (int i = 0; i < childCount; ++i) {
-		const void *child = layoutContext->getChildAt(widget, i);
-		const struct FlexParams *params = layoutContext->getLayoutParams(child);
-		const Align align = getAlign(params);
+		void *child = context->getChildAt(widget, i);
+		struct FlexParams *params = context->getLayoutParams(child);
 		int leadingCrossDim = 0;
-		switch (align) {
+		switch (params->align) {
 			case ALIGN_STRETCH:
 				// Layout the child if the cross size wasn't already definite
 				if (!getStyleSize(params, crossAxis)) {
-					float childWidth = layoutContext->getWidth(child), childHeight = layoutContext->getHeight(child);
+					float childWidth = context->getWidth(child), childHeight = context->getHeight(child);
 					*(crossAxis == DIRECTION_ROW ? &childWidth : &childHeight) = crossSize - getMargin(params, crossAxis);
-					layoutContext->layout(child, childWidth, MEASURE_EXACTLY, childHeight, MEASURE_EXACTLY);
+					context->layout(child, childWidth, MEASURE_EXACTLY, childHeight, MEASURE_EXACTLY);
 				}
 				break;
 			case ALIGN_CENTER:
 			case ALIGN_END:
-				leadingCrossDim += (crossSize - getLayoutSize(layoutContext, child, crossAxis) - getMargin(params, crossAxis)) / (align == ALIGN_CENTER ? 2 : 1);
+				leadingCrossDim = (crossSize - getLayoutSize(context, child, crossAxis) - getMargin(params, crossAxis)) / (params->align == ALIGN_CENTER ? 2 : 1);
+				break;
+			default:
 				break;
 		}
-		(crossAxis == DIRECTION_ROW ? layoutContext->setX : layoutContext->setY)(child, leadingCrossDim + getLeadingMargin(params, crossAxis));
+		(crossAxis == DIRECTION_ROW ? context->setX : context->setY)(child, leadingCrossDim + getLeadingMargin(params, crossAxis));
 	}
 
 	// Set the implicit width and height
-	layoutContext->setWidth(widget, mainAxis == DIRECTION_ROW ? mainSize : crossSize);
-	layoutContext->setHeight(widget, mainAxis == DIRECTION_ROW ? crossSize : mainSize);
+	context->setWidth(widget, mainAxis == DIRECTION_ROW ? mainSize : crossSize);
+	context->setHeight(widget, mainAxis == DIRECTION_ROW ? crossSize : mainSize);
 }
--- flexLayout.h
+++ flexLayout.h
@@ -9,14 +9,13 @@
 extern "C" {
 #endif
 
-#include <stdint.h>
 #include <math.h>
 
 /** The value undefined. */
 #define UNDEFINED NAN
 
 /** Alignments. */
-typedef enum Align {
+enum Align {
 	/** The start margin edge of the item is flushed with the start edge of the line. */
 	ALIGN_START,
 	/** The end margin edge of the item is flushed with the end edge of the line. */
@@ -29,118 +28,116 @@ typedef enum Align {
 	ALIGN_SPACE_AROUND,
 	/** The item fills the line. */
 	ALIGN_STRETCH
-} Align;
+};
 
 /** Layout requirements from the parent. */
-typedef enum MeasureMode {
+enum MeasureMode {
 	/** The parent has not imposed any constraint on the child. */
 	MEASURE_UNSPECIFIED,
 	/** The parent has determined an exact size for the child. */
 	MEASURE_EXACTLY,
 	/** The child can be as large as it wants up to the specified size. */
 	MEASURE_AT_MOST
-} MeasureMode;
+};
 
 /**
  * Returns whether \c value is undefined.
  *
- * @param value the value to check.
- * @return whether \c value is undefined.
+ * @param value The value to check.
+ * @return Whether \c value is undefined.
  */
 int isUndefined(const float value);
 
-/** A layout context that specifies how to query and update properties on widgets. */
-typedef struct LayoutContext {
-	// int (*getX)(void *widget);
+/** A context specifying an interface to the widgets. */
+struct FlexContext {
 	/**
 	 * Sets the x-coordinate of the specified widget.
 	 *
-	 * @param widget the widget.
-	 * @param x the new x-coordinate.
+	 * @param widget The widget.
+	 * @param x The new x-coordinate.
 	 */
 	void (*setX)(const void *widget, float x);
-	// int (*getY)(void *widget);
 	/**
 	 * Sets the x-coordinate of the specified widget.
 	 *
-	 * @param widget the widget.
-	 * @param x the new x-coordinate.
+	 * @param widget The widget.
+	 * @param x The new x-coordinate.
 	 */
 	void (*setY)(const void *widget, float y);
 	/**
 	 * Returns the width set by #setWidth or a layout pass (whichever happened more recently) of the specified widget.
 	 *
-	 * @param widget the widget.
-	 * @return the width of the widget.
+	 * @param widget The widget.
+	 * @return The width of the widget.
 	 */
 	float (*getWidth)(const void *widget);
 	/**
 	 * Sets the width of the widget to the specified value.
 	 *
-	 * @param widget the widget.
-	 * @param width the new width of the widget.
+	 * @param widget The widget.
+	 * @param width The new width of the widget.
 	 */
 	void (*setWidth)(const void *widget, float width);
 	/**
 	 * Returns the height set by #setHeight or a layout pass (whichever happened more recently) of the specified widget.
 	 *
-	 * @param widget the widget.
-	 * @return the height of the widget.
+	 * @param widget The widget.
+	 * @return The height of the widget.
 	 */
 	float (*getHeight)(const void *widget);
 	/**
 	 * Sets the height of the widget to the specified value.
 	 *
-	 * @param widget the widget.
-	 * @param height the new height of the widget.
+	 * @param widget The widget.
+	 * @param height The new height of the widget.
 	 */
 	void (*setHeight)(const void *widget, float height);
 	/**
 	 * Lays out the specified widget.
 	 *
-	 * @param widget the widget to lay out.
-	 * @param width the available width.
-	 * @param widthMode the width requirement.
-	 * @param height the available height.
-	 * @param heightMode the height requirement.
+	 * @param widget The widget to lay out.
+	 * @param width The available width.
+	 * @param widthMode The width requirement.
+	 * @param height The available height.
+	 * @param heightMode The height requirement.
 	 */
-	void (*layout)(const void *widget, float width, MeasureMode widthMode, float height, MeasureMode heightMode);
+	void (*layout)(const void *widget, float width, enum MeasureMode widthMode, float height, enum MeasureMode heightMode);
 	/**
 	 * Returns the number of children of the container.
 	 *
-	 * @param widget the container.
-	 * @return the number of children.
+	 * @param widget The container.
+	 * @return The number of children.
 	 */
 	int (*getChildCount)(const void *widget);
 	/**
 	 * Returns the child of the container at the specified index.
 	 *
-	 * @param widget the container.
-	 * @param index the index.
-	 * @return the child at the index.
+	 * @param widget The container.
+	 * @param index The zero-based index of the child.
+	 * @return The child at the index.
 	 */
 	void *(*getChildAt)(const void *widget, int index);
 	/**
 	 * Returns the layout parameters of the specified widget.
 	 *
-	 * @param widget the widget.
-	 * @return the layout parameters.
+	 * @param widget The widget.
+	 * @return The layout parameters.
 	 */
 	void *(*getLayoutParams)(const void *widget);
-} LayoutContext;
+};
 
-/** Directions in which ::layoutFlex places items. */
-typedef enum FlexDirection {
+/** Directions in which to place items. */
+enum FlexDirection {
 	/** The items are layed out horizontally. */
 	DIRECTION_ROW,
 	/** The items are layed out vertically. */
 	DIRECTION_COLUMN
-} FlexDirection;
+};
 
 /** Options that control how each individual item is layed out. */
-typedef struct FlexParams {
+struct FlexParams {
 	/** The alignment in the container's cross axis. */
-	Align align;
+	enum Align align;
 	/**
 	 * The flex value of the item.
 	 *
@@ -163,24 +160,21 @@ typedef struct FlexParams {
 	float marginBottom;
 	/** The margin space required on the left of the item. */
 	float marginLeft;
-} FlexParams;
+};
 
 /**
  * Lays out the specified flex container.
- * <p>
- * LayoutContext#getLayoutParams when passed a child must return a valid
- * pointer to FlexParams.
  *
- * @param layoutContext the used context.
- * @param widget the flex container.
- * @param width the available width.
- * @param widthMode the width requirement.
- * @param height the available height.
- * @param heightMode the height requirement.
- * @param direction the direction the items are placed in.
- * @param justify the alignment of the content.
+ * @param context The context to use.
+ * @param widget The flex container.
+ * @param width The available width.
+ * @param widthMode The width requirement.
+ * @param height The available height.
+ * @param heightMode The height requirement.
+ * @param direction The direction the items are placed in.
+ * @param justify The alignment of the content.
  */
-void layoutFlex(const struct LayoutContext *layoutContext, const void *widget, float width, enum MeasureMode widthMode, float height, enum MeasureMode heightMode, const FlexDirection direction, const Align justify);
+void layoutFlex(const struct FlexContext *context, void *widget, float width, enum MeasureMode widthMode, float height, enum MeasureMode heightMode, enum FlexDirection direction, enum Align justify);
 
 #ifdef __cplusplus
 }
